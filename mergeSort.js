f523315af940e19b7b73089e39939e7b698d3963
Marcell D <dacmarcell@proton.me>
2023-05-27 18:36:09 -0300

    chore: fixing comments

--- mergeSort.js
+++ mergeSort.js
@@ -3,8 +3,8 @@ let lista = [1,2,5,45,36,4,3653,4,23,6];
 // push() : Adiciona um novo elemento ao final do array
 // Operador condicional terciário : condição ? expressão1 : expressão2
 // shift() : Remove o primeiro elemento do array e retorna ele.
-//Math.floor() : Arredonda o float para baixo e o transforma em inteiro.
-//slice() : Retorna um subarray a partir do array informado.
+// Math.floor() : Arredonda o float para baixo e o transforma em inteiro.
+// slice() : Retorna um subarray a partir do array informado.
 
 function merge (a,b) {
     var result = [];
